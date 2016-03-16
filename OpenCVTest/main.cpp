d4b6daace4aaded07a7c4c623a7ec91fb4a12a25
DannyGotTea <danielgallagher500@gmail.com>
2016-03-16 20:39:12 +0000

    Boilerplate

---
+++ OpenCVTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenCVTest
+//
+//  Created by Daniel Gallagher on 16/03/2016.
+//  Copyright (c) 2016 Developer Co. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, const char * argv[]) {
+    
+}
