296a02b1490e4b4208a50bf98a6452057c98a226
DannyGotTea <danielgallagher500@gmail.com>
2016-05-04 20:10:15 +0100

    Object detection using HSV

--- ObjectRecognition/ObjectRecognition/main.cpp
+++ ObjectRecognition/ObjectRecognition/main.cpp
@@ -10,86 +10,67 @@
 #include <iostream>
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
-using namespace std;
 using namespace cv;
+using namespace std;
 
-void detectAndDisplay(Mat frame);
-
-std::string face_cascade_name =
-"/Users/dannyg/Desktop/OpenCV/ObjectRecognition/Resources/haarcascade_frontalface_alt.xml";
-
-std::string eyes_cascade_name =
-"/Users/dannyg/Desktop/OpenCV/ObjectRecognition/Resources/haarcascade_eye_tree_eyeglasses.xml";
-
-CascadeClassifier face_cascade;
-CascadeClassifier eyes_cascade;
-
-std::string windowName = "DannyG's Face Detection"; 
- 
-int main( void )
+int main(int argc, char** argv)
 {
-    VideoCapture capture;
-    Mat frame;
+    VideoCapture cap(0); //capture the video from webcam
     
-    //-- 1. Load the cascades
-    if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face cascade\n"); return -1; };
-    if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading eyes cascade\n"); return -1; };
-    
-    //-- 2. Read the video stream
-    capture.open( -1 );
-    if ( ! capture.isOpened() ) { printf("--(!)Error opening video capture\n"); return -1; }
-    
-    while (  capture.read(frame) )
+    if (!cap.isOpened())
     {
-        if( frame.empty() )
-        {
-            printf(" --(!) No captured frame -- Break!");
-            break;
-        }
-        
-        //-- 3. Apply the classifier to the frame
-        detectAndDisplay( frame );
-        
-        int c = waitKey(10);
-        if( (char)c == 27 ) { break; } // escape
+        cout << "Cannot open the webcam" << endl;
+        return -1;
     }
-    return 0;
+    
+    namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called control
+    
+    int iLowH = 0, iHighH = 179;
+    
+    int iLowS = 0, iHighS = 255;
+    
+    int iLowV = 0, iHighV = 255;
+    
+    cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+    cvCreateTrackbar("HighH", "Control", &iHighH, 179);
+    
+    cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+    cvCreateTrackbar("HighS", "Control", &iHighS, 255);
+    
+    cvCreateTrackbar("LowV", "Control", &iLowH, 255); //Hue (0 - 255)
+    cvCreateTrackbar("HighV", "Control", &iHighH, 255);
+    
+    while(true)
+    {
+        Mat imgOriginal;
+        
+        bool bSuccess = cap.read(imgOriginal); //read a new frame from the video
+        
+        if(!bSuccess) //if not success, break loop
+        {
+            
+        }
+    }
+    
+    
 }
 
-void detectAndDisplay( Mat frame )
-{
-    std::vector<Rect> faces;
-    Mat frame_gray;
-    
-    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
-    equalizeHist( frame_gray, frame_gray );
-    
-    //-- Detect faces
-    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
-    
-    for( size_t i = 0; i < faces.size(); i++ )
-    {
-        Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
-        ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
-        
-        Mat faceROI = frame_gray( faces[i] );
-        std::vector<Rect> eyes;
-        
-        //-- In each face, detect eyes
-        eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
-        
-        for( size_t j = 0; j < eyes.size(); j++ )
-        {
-            Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
-            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-            circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
-        }
-        waitKey(30);
-    }
-    //-- Show what you got
-    imshow( windowName, frame );
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
